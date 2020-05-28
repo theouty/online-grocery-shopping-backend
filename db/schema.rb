aa84a80637e7cc89b334a2ded702a191219fa3a7
dizhengcindy <dizhengcindy@gmail.com>
2020-05-28 09:19:33 -0700

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_195358) do
+ActiveRecord::Schema.define(version: 2020_05_25_195454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,6 @@ ActiveRecord::Schema.define(version: 2020_05_25_195358) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-<<<<<<< HEAD
-=======
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.float "price"
@@ -35,7 +33,6 @@ ActiveRecord::Schema.define(version: 2020_05_25_195358) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
->>>>>>> master
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "address"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-wheat_thins = Item.create(name: "Wheat Thins", category: "Snacks", img_url: "https://i.imgur.com/a1cLXfi.jpg", price: 3.44)
-tostitos = Item.create(name: "Tostitos", category: "Snacks", img_url: "https://i.imgur.com/0ZOjTo7.jpg", price: 3.98)
-original_ruffles = Item.create(name: "Ruffles - Original", category: "Snacks", img_url: "https://i.imgur.com/LKR5YUP.jpg", price: 3.98)
-ritz_crackers = Item.create(name: "Ritz", category: "Snacks", img_url: "https://i.imgur.com/9xbEfyH.jpg", price: 2.68)
-oreo = Item.create(name: "Oreo", category: "Snacks", img_url: "https://i.imgur.com/9FQbVag.jpg", price: 2.96)
-fritos = Item.create(name: "Fritos", category: "Snacks", img_url: "https://i.imgur.com/AGzXmkR.jpg", price: 3.28)
-red_bell_pepper = Item.create(name: "Red Bell Pepper", category: "Produce", img_url: "https://i.imgur.com/LWHra2y.jpg", price: 1.38)
-doritos = Item.create(name: "Doritos", category: "Snacks", img_url: "https://i.imgur.com/T3siVrT.jpg", price: 3.98)
-cheddar_ruffles = Item.create(name: "Ruffles - Cheddar & Sour Cream", category: "Snacks", img_url: "https://i.imgur.com/L7Uedyp.jpg", price: 2.98)
-strawberries = Item.create(name: "Strawberries", category: "Produce", img_url: "https://i.imgur.com/w4Xrvlb.jpg", price: 3.39)
-butter = Item.create(name: "Kerrygold Pure Irish Salted Butter", category: "Dairy", img_url: "https://i.imgur.com/dBrET2Q.jpg", price: 3.99)
-potato = Item.create(name: "Russet Potato", category: "Produce", img_url: "https://i.imgur.com/kqW7iv9.jpg", price: 0.67)
-grapes = Item.create(name: "Red Seedless Grapes", category: "Produce", img_url: "https://i.imgur.com/rLiFIb0.jpg", price: 5.98)
-onion = Item.create(name: "Yellow Onion", category: "Produce", img_url: "https://i.imgur.com/2Dw70Gj.jpg", price: 0.72)
-turkey = Item.create(name: "Sliced Honey Roasted Turkey", category: "Meat", img_url: "https://i.imgur.com/LmjReRu.jpg", price: 4.98)
-apple = Item.create(name: "Gala Apple", category: "Produce", img_url: "https://i.imgur.com/ho4n8Fq.jpg", price: 1.35)
-ham = Item.create(name: "Sliced Honey Ham", category: "Meat", img_url: "https://i.imgur.com/RV96f5R.jpg", price: 3.28)
-ground_beef = Item.create(name: "80% Lean Ground Beef", category: "Meat", img_url: "https://i.imgur.com/cpHJjSs.jpg", price: 4.39)
-chicken = Item.create(name: "Chicken Breast Fillets", category: "Meat", img_url: "https://i.imgur.com/gFTkiq1.jpg", price: 5.99)
-hotdogs = Item.create(name: "Ball Park Franks", category: "Meat", img_url: "https://i.imgur.com/Qmxi42A.jpg", price: 3.49)
-sourcream = Item.create(name: "Daisy Sour Cream", category: "Dairy", img_url: "https://i.imgur.com/yjiefhp.jpg", price: 1.77)
-bacon = Item.create(name: "Hormel Bacon", category: "Meat", img_url: "https://i.imgur.com/cNPh8az.jpg", price: 5.00)
-mozzarella = Item.create(name: "Shredded Mozzarella Cheese", category: "Dairy", img_url: "https://i.imgur.com/u0GdkoU.jpg", price: 5.99)
-milk = Item.create(name: "Milk", category: "Dairy", img_url: "https://i.imgur.com/JdCvsTx.jpg", price: 4.49)
-eggs = Item.create(name: "Eggs", category: "Dairy", img_url: "https://i.imgur.com/eO5FYqQ.jpg", price: 2.79)
-cream_cheese = Item.create(name: "Philadelphia Original Cream Cheese", category: "Dairy", img_url: "https://i.imgur.com/dcYqZF6.jpg", price: 1.96)
-mexican_cheese = Item.create(name: "Shredded Mexican Four Cheese Blend", category: "Dairy", img_url: "https://i.imgur.com/uqi8Jzi.jpg", price: 2.19)
-almond_breeze = Item.create(name: "Almond Breeze Almondmilk", category: "Dairy", img_url: "https://i.imgur.com/1OgYyf8.jpg", price: 3.77)
-sargento = Item.create(name: "Sargento Medium Cheddar Cheese Slices", category: "Dairy", img_url: "https://i.imgur.com/QrFc3Rc.jpg", price: 2.68)
-
-
-=======
 wheat_thins = Item.create(description: "Pair snack crackers with cheese and meat or enjoy by themselves.", name: "Wheat Thins", category: "Snacks", img_url: "https://i.imgur.com/a1cLXfi.jpg", price: 3.44)
 tostitos = Item.create(description: "It's like a mini chip bowl in every bite.", name: "Tostitos Scoops", category: "Snacks", img_url: "https://i.imgur.com/0ZOjTo7.jpg", price: 3.98)
 original_ruffles = Item.create(description: "Classic tasty flavor of RUFFLES Potato Chips.", name: "Ruffles - Original", category: "Snacks", img_url: "https://i.imgur.com/LKR5YUP.jpg", price: 3.98)
@@ -78,4 +38,3 @@ diet_coke = Item.create(description: "Diet Coke is a delicious, crisp tasting, n
 diet_dp = Item.create(description: "Zero calories per 8 ounce serving.", name: "Diet Dr. Pepper", category: "Beverages", price: 5.28, img_url: "https://i.imgur.com/QuGpZTL.jpg")
 ovc = Item.create(description: "For crips, cold taste, serve chilled at 37 degrees.", name: "Coca-Cola-Zero Orange Vanilla", category: "Beverages", price: 5.89, img_url: "https://i.imgur.com/lgaxtjg.jpg")
 rootbeer = Item.create(description: "Good since 1898.", name: "Barq's Root Beer Soda", category: "Beverages", price: 5.89, img_url: "https://i.imgur.com/lquUiVL.jpg")
->>>>>>> master
