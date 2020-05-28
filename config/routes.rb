38ba8ac7ed70d0b27dee018932d6b3abc5582f4f
dizhengcindy <dizhengcindy@gmail.com>
2020-05-28 11:18:35 -0700

    added routes and change in cartitem index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :items, only: [:index, :show]
-  resources :cart_items, only: [:index, :create, :update, :destroy]
+  resources :cart_items, only: [:create, :update, :destroy]
   resources :users, only: [:show, :create, :update, :destroy]
   post '/auth', to: 'auth#create'
+ 
+  get '/cart_items/:user_id', to: 'cart_items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
